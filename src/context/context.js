0b9e819955b642ed6da91bd0a4406f17955f326d
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2023-04-13 14:36:25 +0530

    useContext

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+export const GithubContext = React.createContext();
+
+export const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value="Sam">{children}</GithubContext.Provider>
+  );
+};
