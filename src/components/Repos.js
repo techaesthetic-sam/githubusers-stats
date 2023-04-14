5ca9262dd2d0a25f262810a2ade6c3b77829778e
techaesthetic-sam <85981274+techaesthetic-sam@users.noreply.github.com>
2023-04-14 20:27:37 +0530

    pie dynamic

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,50 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useContext(GithubContext);
+  // console.log(repos);
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    // total[language] = 30;
+    // console.log(total[language]);
+    if (!total[language]) total[language] = { label: language, value: 1 };
+    else
+      total[language] = {
+        label: language,
+        value: (total[language].value += 1),
+      };
+    return total;
+  }, {});
+
+  languages = Object.values(languages).sort((a, b) => a.value > b.value);
+  // console.log(languages); // this is where it returns everything
+  const chartData = [
+    {
+      label: "HTML",
+      value: "290",
+    },
+    {
+      label: "CSS",
+      value: "260",
+    },
+    {
+      label: "JAVASCRIPT",
+      value: "580",
+    },
+  ];
+  return (
+    <section>
+      <Wrapper className="section-center">
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={languages} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
